4b4d16bf2a7bfe9a37428e2e9ad700ed4ba65cf7
IceCream0910 <44636839+IceCream0910@users.noreply.github.com>
2019-12-04 16:17:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function access() {
  var codename = document.getElementById("codename").value;
  var password = document.getElementById("password").value;
 
- if(codename=="icecream" && password=="nationalgarden") {
+ if(codename=="icecream" && password=="inhababo") {
    window.open("main/index.html","_self")
  } else {
   window.open("index.html","_self")
